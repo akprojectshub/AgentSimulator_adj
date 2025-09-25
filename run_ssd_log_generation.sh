e3d6aed3a69e805d901048e9ec047f420788b608
Alexander <alexander.kraus@informatik.uni-mannheim.de>
2025-09-25 23:34:40 +0200

    Update bash file

--- run_ssd_log_generation.sh
+++ run_ssd_log_generation.sh
@@ -2,9 +2,8 @@
 
 #SBATCH --job-name=ssd_log_generation      # Descriptive job name
 #SBATCH --ntasks=1                         # One task (one Python process)
-#SBATCH --cpus-per-task=10                 # XX CPU threads for that process
+#SBATCH --cpus-per-task=60                 # XX CPU threads for that process
 #SBATCH --mem=20G                          # Total memory for the job
-#SBATCH --time=12:00:00                    # Wall time limit
 #SBATCH --partition=cpu                    # Use the 'cpu' partition
 #SBATCH --output=logs/%x_%j.out            # Save stdout to logs/jobname_jobid.out
 #SBATCH --error=logs/%x_%j.err             # Save stderr to logs/jobname_jobid.err
@@ -20,4 +19,4 @@ mkdir -p logs
 
 # Run Python evaluation script with explicitly passed CPU count
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-python simulate.py --log_path raw_data/experiment_1_settings/experiment_1_bimp_log.csv --case_id case_id --activity_name activity --resource_name resource --end_timestamp end_time --start_timestamp start_time --num_simulations 10 --central_orchestration --num_cores=${SLURM_CPUS_PER_TASK}
+python simulate.py --log_path raw_data/experiment_1_settings/original_model/experiment_1_bimp_log.csv --case_id case_id --activity_name activity --resource_name resource --end_timestamp end_time --start_timestamp start_time --num_simulations 30 --central_orchestration --num_cores=${SLURM_CPUS_PER_TASK}
